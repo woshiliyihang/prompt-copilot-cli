4d532a438000f586799b115f95b9b1ed2c10446c
Cornex <mhh.you@163.com>
2026-08-01 17:09:04 +0800

    更新命令工具

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ def t(key: str, **kwargs: Any) -> str:
 
 
 UI_SYSTEM_LANGUAGE = "en"
-APPLICATION_VERSION = "0.1.8"
+APPLICATION_VERSION = "0.1.9"
 DEFAULT_SYSTEM_PROMPT = t("system_prompt")
 WORKSPACE_DIR = ROOT / "workspace"
 LOG_DIR = ROOT / "logs"
