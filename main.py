c3ff28cef17f1125f8e7023789ddecfa75037541
Cornex <mhh.you@163.com>
2026-08-06 11:34:19 +0800

    发布0.2.3版本

--- main.py
+++ main.py
@@ -305,7 +305,7 @@ def t(key: str, **kwargs: Any) -> str:
 
 
 UI_SYSTEM_LANGUAGE = "en"
-APPLICATION_VERSION = "0.2.2"
+APPLICATION_VERSION = "0.2.3"
 DEFAULT_SYSTEM_PROMPT = t("system_prompt")
 WORKSPACE_DIR = ROOT / "workspace"
 LOG_DIR = ROOT / "logs"
